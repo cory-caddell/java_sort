c36ab70f63d5066ab30a4358446a5a2c54b5e065
cdc-001 <cory.caddell@gmail.com>
2023-03-01 14:29:00 -0600

    add print method

--- CoryCaddellAssignment2B.java
+++ CoryCaddellAssignment2B.java
@@ -1,4 +1,29 @@
+// CoryCaddellAssignment2B.java
+
+import java.util.Scanner;
 
 public class CoryCaddellAssignment2B {
+	
+	public static void main(String[] args) {
+		
+		// delete me!!!
+		int[] array = {8, 3, 2, 6, 0, 7, 1, 5, 4, 9};
+		
+		// get array
+		// copy array
+		// display array to console
+		CoryCaddellPrintArray(array);
+		// sort using insertion sort algorithm
+		// sort using bubble sort algorithm
+		// sort using merge sort algorithm
+		// sort using quick sort algorithm
+	}
+	
+	/** Method to print elements of array. */
+	public static void CoryCaddellPrintArray(int[] array) {
+		for (int element : array) {
+			System.out.print(element + "\t");
+		}
+	}
 
 }
